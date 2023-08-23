9313170b55a12fd5510a3b65b419a605d5e40e76
Igor <100830659+igornastt@users.noreply.github.com>
2023-08-23 18:53:27 +0300

    Update forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -15,7 +15,8 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != "is_current_version":
+                field.widget.attrs['class'] = 'form-control'
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
