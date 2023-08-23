70d283328c78f45d33fe50ca3ec1e877d0481571
igornast <grezle00@gmail.com>
2023-08-23 11:47:56 +0300

    Изменение файлов

--- catalog/models.py
+++ catalog/models.py
@@ -8,11 +8,11 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    #preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
     category = models.CharField(max_length=100, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
-    creation_date = models.DateTimeField(verbose_name='дата создания')
-    change_date = models.DateTimeField(verbose_name='дата последнего изменения')
+    #creation_date = models.DateTimeField(verbose_name='дата создания')
+    #change_date = models.DateTimeField(verbose_name='дата последнего изменения')
 
     def __str__(self):
         return f'{self.name}, {self.category}, {self.price}'
