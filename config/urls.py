268cd997a1097cc99941d2f41931b196144ce5c9
igornast <grezle00@gmail.com>
2023-08-27 15:34:21 +0300

    Изменение файлов

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
-    path('', include('contacts.urls')),
-    path('blog/', include('blog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path("admin/", admin.site.urls),
+                  path('', include('catalog.urls')),
+                  path('blog/', include('blog.urls', namespace='blog')),
+                  path('users/', include('users.urls', namespace='user')),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
