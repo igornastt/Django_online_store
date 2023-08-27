faaceb16b0230b849f87c5a7288a693d140cf122
igornast <grezle00@gmail.com>
2023-08-27 15:32:25 +0300

    Создано новое приложение users

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
 
 
-class ContactsConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contacts'
+    name = 'users'
     verbose_name = 'контакты'
+
+
+class UsersConfig:
+    name = 'users'
+    
