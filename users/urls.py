43ae63b45b371c0c5c84aeb1364d0a60cdba137a
Igor <100830659+igornastt@users.noreply.github.com>
2023-08-28 11:50:14 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
 ]
