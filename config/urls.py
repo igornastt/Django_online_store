ee7fbab79fc742cbf23619cff64328f9bbf1e3ae
Igor <100830659+igornastt@users.noreply.github.com>
2023-09-01 16:55:44 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path("users/", include("users.urls", namespace="users")),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('contacts.urls')),
+    path('blog/', include('blog.urls')),
+    path('users/', include('users.urls', namespace='users'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
