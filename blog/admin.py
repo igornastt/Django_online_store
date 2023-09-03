0f60432995e0f54b3cd9e799a41e972ecf130cf1
Igor <100830659+igornastt@users.noreply.github.com>
2023-09-03 13:18:13 +0300

    Delete blog directory

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from blog.models import Blog
-
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    """Представление раздела - блог в админке"""
-
-    list_display = ('title',)
