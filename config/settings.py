6f2859370468a56beebc2e4897974e76729294d2
Igor <100830659+igornastt@users.noreply.github.com>
2023-09-05 08:47:21 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -159,9 +159,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
     }
-}
